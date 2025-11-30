1c988031b691335f82be3541f89a28de5f22cbb0
gayfemboy276 <gayfemboy276@gmail.com>
2025-11-30 02:34:01 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# detect the folder where this installer is at
+
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+FOLDER_NAME="$(basename "$DOTFILES_DIR")"
+
+# Check if folder name is "dotfiles"
+if [ "$FOLDER_NAME" != "dotfiles" ]; then
+    echo "ey bro, rename the folder to dotfiles so this works (very important)
+    echo "current folder name: '$FOLDER_NAME'"
+    exit 1
+fi
+
+echo "folder name verified (VERY IMPORTANT)"
+
+# make sure its runnin wit sudo
+if [ "$EUID" -ne 0 ]; then
+    echo "yo bro run this wit sudo so pacman works"
+    echo "here run ts: sudo ./install.sh"
+    exit 1
+fi
+
+
+set -e  # exit on other error
+
+#  install the needed packages
+echo "installing all needed packages"
+sudo pacman -S hyprland hyprpaper cava wofi
+
+
+# create config folders if they dont exist
+echo "Creating config directories..."
+mkdir -p ~/.config/hyprland
+mkdir -p ~/.config/wofi
+mkdir -p ~/.config/cava
+
+# --- 3. Copy or symlink dotfiles ---
+echo "done, copying dotfiles to their corresponding folders"
+
+# hyprland
+cp -rf ~/dotfiles/hyprland/* ~/.config/hyprland/
+
+# wofi
+cp -rf ~/dotfiles/wofi/* ~/.config/wofi/
+
+# Cava
+cp -rf ~/dotfiles/cava/* ~/.config/cava/
+
+# Bashrc
+cp -f ~/dotfiles/shell/.bashrc ~/.bashrc
+
+# optional: set permissions (just in case)
+chmod -R 644 ~/.config/hyprland/*
+chmod -R 644 ~/.config/wofi/*
+chmod -R 644 ~/.config/kitty/*
+chmod -R 644 ~/.config/cava/*
+chmod 644 ~/.bashrc
+
+echo "setup complete, make sure to set an image for hyprpaper in $HOME/.config/hypr/hyprpaper.conf"
+echo "so hyprpaper doesnt be a bitch"
